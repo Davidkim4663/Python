a1a57e00c4c6df19ef83735acdfd0c358c7a3882
David <ip4523@naver.com>
2024-08-20 20:00:27 +0900

    test

---
+++ day03_0820/day02_review.py
@@ -0,0 +1,20 @@
+# 자료와 데이터
+# 리터럴(값 자체) :정수, 실수, 문자열, 불리언, 타입/자료형으로 구분한 자료들
+#  정수 : 10,23,454 etc
+#  실수 : 3.14 etc
+#  문자열 : "안녕"
+
+#  변수 : 하나의 자료를 저장하는 메모리 공간
+temp = 0
+a = 10
+b = 20
+print(temp)
+print(a)
+print(b)
+
+temp = a
+a = b
+b = temp
+print("temp = " + str(temp))
+print("a = " + str(a))
+print("b = " + str(b))
