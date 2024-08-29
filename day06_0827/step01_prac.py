41a14fb707c19b7591d77cf83d97019e08c1fc89
David <ip4523@naver.com>
2024-08-29 17:25:20 +0900

    test

---
+++ day06_0827/step01_prac.py
@@ -0,0 +1,14 @@
+# 리스트 [] : 어떤 자료의 값을 일정한 순서로 적은 것
+
+# 리스트의 인덱싱과 슬라이싱
+student = ['유재석', '신동엽', '강호동', '서장훈']
+
+# 슬라이싱 : 인덱싱을 이용한 요소들 추출 index - 1
+# print(student[0 : 2]) # 0 ~ 1
+# print(student[0 : 3]) # 0 ~ 2
+# print(student[ : 4]) # index 마지막 요소 전까지 슬라이싱
+# print(student[2 : ])
+# print(student[ : ])
+# print(student[0 : 4 : 1]) #0부터 3까지 1씩 증가
+# print(student[-1 : -5 : -1]) # -1부터 -4까지 1씩 증가
+# print(student[-1 : -5 : -2]) # -1부터 -4까지 2씩 증가
